61e9076655d09199a98d5a67b0ddb941e8b5d161
Théophile ROUGET <theophile.rouget@edu.devinci.fr>
2020-12-06 18:29:46 +0100

    début classe plateau

--- Plateau.cs
+++ Plateau.cs
@@ -8,5 +8,56 @@ namespace Prob
 {
     class Plateau
     {
+        private De[] des = new De[16];
+        private char[,] lettresTirees = new char[4,4];
+
+        public Plateau(De[] des, char[,] lettresTirees)
+        {
+            this.des = des;
+            this.lettresTirees = lettresTirees;
+        }
+
+        public De[] Des
+        {
+            get { return this.des; }
+            set { this.des = value; }
+        }
+        public char[,] LettresTirees
+        {
+            get { return this.lettresTirees; }
+            set { this.lettresTirees = value; }
+        }
+
+        
+
+        /// <summary>
+        /// Affichge de l'état du plateau  <=> affiche les 16 lettres tirées sous forme d'une matrice 4*4
+        /// </summary>
+        /// <returns> return un string affiché sous forme de matricestring</returns>
+        public override string ToString()
+        {
+            string chaine = "";
+
+            for(int i=0; i<this.des.Length; i++)
+            {
+                for (int j = 0; j < this.des.Length; j++)
+                {
+                    chaine += this.LettresTirees[i, j] + " ";
+                }
+                chaine += "\n";
+            }
+            return chaine;
+        }
+
+        public bool Test_Plateau(string mot)
+        {
+            bool eligible = false;
+
+
+
+
+            return eligible;
+        }
+
     }
 }
