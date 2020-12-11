c188b9b427848cb1dbb5ac4b77ab5bac2deb5ca9
Théophile ROUGET <theophile.rouget@edu.devinci.fr>
2020-12-12 00:39:28 +0100

    readfile débile mais fonctionnel

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -94,37 +94,137 @@ namespace Prob
         public void ReadFile(StreamReader sReader)
         {
             string line;
-            char[] separateur = { ' ' };
-            int nbLettres = -1;
+            char separateur = ' ';
+            //int nbLettres = -1;
             List<string> listeTempo = new List<string>();
+
+
+            List<string> liste2 = new List<string>();
+            List<string> liste3 = new List<string>();
+            List<string> liste4 = new List<string>();
+            List<string> liste5 = new List<string>();
+            List<string> liste6 = new List<string>();
+            List<string> liste7 = new List<string>();
+            List<string> liste8 = new List<string>();
+            List<string> liste9 = new List<string>();
+            List<string> liste10 = new List<string>();
+            List<string> liste11 = new List<string>();
+            List<string> liste12 = new List<string>();
+            List<string> liste13 = new List<string>();
+            List<string> liste14 = new List<string>();
+            List<string> liste15 = new List<string>();
 
             try
             {
+
                 while ((line = sReader.ReadLine()) != null)
                 {
 
+                    string[] tab = line.Split(separateur);
 
-                    if (VerifChiffre(line) == true) // si on detecte un nombre : on met listeTempo dans notre SortedList et on transforme le string detecté en entier pour ensuite l'assigner à un indexeur de notre sortedList 
+                    foreach (string mot in tab)
                     {
-                        if (nbLettres >= 1) //pour eviter le 1er tour de boucle
+                        if (VerifChiffre(line) == false)
                         {
-                            this.ensembleMot.Add(nbLettres, listeTempo);
-                            listeTempo.Clear();
+                            //listeTempo.Add(mot); //liste avec tous les mots
+                            Console.WriteLine(mot);
+                            if (mot.Length == 2)
+                            {
+                                liste2.Add(mot);
+                                Console.WriteLine(mot);
+                            }
+                            else if (mot.Length == 3)
+                            {
+                                liste3.Add(mot);
+                            }
+                            else if (mot.Length == 4)
+                            {
+                                liste4.Add(mot);
+                            }
+                            else if (mot.Length == 5)
+                            {
+                                liste5.Add(mot);
+                            }
+                            else if (mot.Length == 6)
+                            {
+                                liste6.Add(mot);
+                            }
+                            else if (mot.Length == 7)
+                            {
+                                liste7.Add(mot);
+                            }
+                            else if (mot.Length == 8)
+                            {
+                                liste8.Add(mot);
+                            }
+                            else if (mot.Length == 9)
+                            {
+                                liste9.Add(mot);
+                            }
+                            else if (mot.Length == 10)
+                            {
+                                liste10.Add(mot);
+                            }
+                            else if (mot.Length == 11)
+                            {
+                                liste11.Add(mot);
+                            }
+                            else if (mot.Length == 12)
+                            {
+                                liste12.Add(mot);
+                            }
+                            else if (mot.Length == 13)
+                            {
+                                liste13.Add(mot);
+                            }
+                            else if (mot.Length == 14)
+                            {
+                                liste14.Add(mot);
+                            }
+                            else if (mot.Length == 15)
+                            {
+                                liste15.Add(mot);
+                            }
                         }
 
-                        nbLettres = int.Parse(line);
-                    }
-                    else // si ce n'est pas un chiffre : on sépare dans un tableau chaque mot lu et on copie les données de ce tableau dans une liste, que l'on ajoute ensuite à la sorted liste avec l'indexeur associé
-                    {
-                        string[] tab = line.Split(separateur);
-
-                        foreach (string mot in tab)
-                        {
-                            listeTempo.Add(mot);
-                        }
                     }
                 }
 
+                this.ensembleMot.Add(2, liste2);
+                this.ensembleMot.Add(3, liste3);
+                this.ensembleMot.Add(4, liste4);
+                this.ensembleMot.Add(5, liste5);
+                this.ensembleMot.Add(6, liste6);
+                this.ensembleMot.Add(7, liste7);
+                this.ensembleMot.Add(8, liste8);
+                this.ensembleMot.Add(9, liste9);
+                this.ensembleMot.Add(10, liste10);
+                this.ensembleMot.Add(11, liste11);
+                this.ensembleMot.Add(12, liste12);
+                this.ensembleMot.Add(13, liste13);
+                this.ensembleMot.Add(14, liste14);
+                this.ensembleMot.Add(15, liste15);
+
+                //while ((line = sReader.ReadLine()) != null)
+                //{
+                //    if (VerifChiffre(line) == true) // si on detecte un nombre : on met listeTempo dans notre SortedList et on transforme le string detecté en entier pour ensuite l'assigner à un indexeur de notre sortedList 
+                //    {
+                //        if (nbLettres >= 1) //pour eviter le 1er tour de boucle
+                //        {
+                //            this.ensembleMot.Add(nbLettres, listeTempo);
+                //            listeTempo.Clear();
+                //        }
+                //        nbLettres = int.Parse(line);
+                //    }                    
+                //    else // si ce n'est pas un chiffre : on sépare dans un tableau chaque mot lu et on copie les données de ce tableau dans une liste, que l'on ajoute ensuite à la sorted liste avec l'indexeur associé
+                //    {
+                //        string[] tab = line.Split(separateur); 
+                //        foreach(string mot in tab)
+                //        {
+                //            listeTempo.Add(mot);                          
+                //        }
+                //    }                    
+                //}
             }
             catch (Exception e)
             {
@@ -135,7 +235,6 @@ namespace Prob
                 if (sReader != null) sReader.Close();
             }
         }
-
         //recherche en récursif;
         public bool RechDichoRecursif(int debut, int fin, string mot)
         {
